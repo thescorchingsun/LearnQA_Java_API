c903dcf31fb3a591ae9dfd75a5cd5b4a71403928
thescorchingsun <qbtestbq@gmail.com>
2023-04-23 13:28:18 +0200

    Добавлен метод для проверки GET запроса

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
 
     @Test
-    public  void helloWorldTest(){
+    public void helloWorldTest() {
         System.out.println("Hello from Natalia");
+    }
+
+    @Test
+    public void getHelloWorldTest(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
