c8ffc23a6c8d6f78c0320350e6956530d67ade24
thescorchingsun <qbtestbq@gmail.com>
2023-05-15 21:10:26 +0200

    Ex7_LongRedirect

--- src/test/java/Ex7_LongRedirect.java
+++ src/test/java/Ex7_LongRedirect.java
@@ -4,6 +4,35 @@ import org.junit.jupiter.api.Test;
 
 public class Ex7_LongRedirect {
 
+    @Test
+    public void LongRedirectTest() {
 
+        String link = "https://playground.learnqa.ru/api/long_redirect";
+        int redirects = 0;
 
+        do {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(link)
+                    .andReturn();
+
+            int statusCode = response.getStatusCode();
+            if (statusCode == 200) {
+                System.out.println(response.getHeader("Location"));
+                System.out.println(statusCode);
+                break;
+            } else {
+                link = response.getHeader("Location");
+                redirects++;
+                System.out.println(response.getHeader("Location"));
+                System.out.println(statusCode);
+            }
+        } while (link != null);
+
+        System.out.println(redirects);
+
+    }
 }
