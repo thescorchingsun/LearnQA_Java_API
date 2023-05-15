d6da0b0cdedc2505e7e273f7b39bd22630d148e2
thescorchingsun <qbtestbq@gmail.com>
2023-05-15 21:56:10 +0200

    Ex10_ShortPhrase

--- src/test/java/Ex10_ShortPhrase.java
+++ src/test/java/Ex10_ShortPhrase.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Ex10_ShortPhrase {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class Ex10_ShortPhrase {
+
+    @Test
+    public void lengthCheckTest() {
+
+        String text = "воздухоплаватель";
+
+        int letterCount = text.length();
+
+        assertTrue(letterCount > 15,"Ошибка - текст равен " + letterCount + " символам");
+        System.out.println("Успех - текст длиннее 15 символов");
+    }
 }
